b8d730e8aa6535412aa802c27e4ef1f3e034f4c2
Sivaranjani-A <ranjani731997@gmail.com>
2023-03-01 21:57:53 +0530

    update resume

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -1,9 +1,11 @@
 import "./HeroimgStyles.css";
 import IntroImg from "../assets/intro-bg.jpg"
+import resume from "../assets/sivaranjani resume.pdf"
 
 
 import React from 'react'
 import { Link } from "react-router-dom";
+import { FaDownload } from "react-icons/fa";
 
 const Heroimg = () => {
     return (
@@ -16,7 +18,7 @@ const Heroimg = () => {
                 <h1>FullStack Developer.</h1>
                 <div>
                     <Link to={"/project"} className="btn">Projects</Link>
-                    <Link to={"/contact"} className="btn btn-light">Contact</Link>
+                    <a href={resume} download=""><button class="btn"> download CV <FaDownload size={16} style={{ color: "#fff", marginRight: "1rem" }} /></button></a>
                 </div>
             </div>
         </div>
