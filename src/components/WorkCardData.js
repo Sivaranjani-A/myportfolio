770786a828193c6a26b0e098802a7ed05f44f691
Sivaranjani-A <ranjani731997@gmail.com>
2023-03-31 10:46:17 +0530

    update

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -6,25 +6,26 @@ import pro3 from "../assets/project3.png";
 
 const ProjectCardData = [
     {
-        imgsrc: pro1,
-        title: "URL SHORTENING APPLICATION",
-        text: "It’s an url shorten app user can login and register the account.User enter the long url and it converts into the short url.Whenever the user click on the short url it will redirect to the long url page",
-        view: "https://resilient-alfajores-4a4f33.netlify.app/",
-        source: "https://github.com/Sivaranjani-A/urlshortener-frontend"
-    },
-    {
         imgsrc: pro2,
         title: "MOVIE APP",
         text: "The purpose of the movie app is user can add movie,edit movie,delete movie. All the CRUD operations are performed.",
         view: "https://rococo-kulfi-f09a3a.netlify.app/",
         source: "https://github.com/Sivaranjani-A/react-movie-poster"
     },
+
     {
         imgsrc: pro3,
         title: "LIBRARY MANAGEMENT",
         text: "It is a simple library management app.Login as a public,can view books,borrow books and return it.Login as a librarian add books,view customer details.",
         view: "https://splendorous-cocada-ab437d.netlify.app/",
         source: "https://github.com/Sivaranjani-A/react-library_management"
-    }
+    },
+    {
+        imgsrc: pro1,
+        title: "STACK OVERFLOW CLONE",
+        text: "It’s a simple stack overflow clone app, user can search answers and vote answers and also post answers using keyword",
+        view: "https://beautiful-manatee-18088e.netlify.app/",
+        source: "https://github.com/Sivaranjani-A/stackoverflowclone-frontend"
+    },
 ]
 export default ProjectCardData;
