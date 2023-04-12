0186b5b21b9847391ae3cef0f24411c213649dde
Sivaranjani-A <ranjani731997@gmail.com>
2023-04-12 21:32:15 +0530

    update

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,23 +1,27 @@
 import "./WorkCardStyles.css";
 import pro1 from "../assets/project1.png";
 
-import React from 'react'
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 const WorkCard = ({ imgsrc, title, text, view, source }) => {
-    return (
-        <div className="project-card">
-            <img src={imgsrc} alt="image" />
-            <h2 className="project-title">{title}</h2>
-            <div className="pro-details">
-                <p>{text}</p>
-                <div className="pro-btns">
-                    <NavLink to={view} className="btn">View</NavLink>
-                    <NavLink to={source} className="btn">Source</NavLink>
-                </div>
-            </div>
+  return (
+    <div className="project-card">
+      <img src={imgsrc} alt="image" />
+      <h2 className="project-title">{title}</h2>
+      <div className="pro-details">
+        <p>{text}</p>
+        <div className="pro-btns">
+          <NavLink to={view} className="btn" target="blank">
+            View
+          </NavLink>
+          <NavLink to={source} className="btn" target="blank">
+            Source
+          </NavLink>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default WorkCard
+export default WorkCard;
--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -3,37 +3,35 @@ import pro2 from "../assets/project2.png";
 import pro3 from "../assets/project3.png";
 import pro4 from "../assets/project4.png";
 
-
-
 const ProjectCardData = [
-    {
-        imgsrc: pro2,
-        title: "MOVIE APP",
-        text: "The purpose of the movie app is user can add movie,edit movie,delete movie. All the CRUD operations are performed.",
-        view: "https://rococo-kulfi-f09a3a.netlify.app/",
-        source: "https://github.com/Sivaranjani-A/react-movie-poster"
-    },
+  {
+    imgsrc: pro2,
+    title: "MOVIE APP",
+    text: "The purpose of the movie app is user can add movie,edit movie,delete movie. All the CRUD operations are performed.",
+    view: "https://rococo-kulfi-f09a3a.netlify.app/",
+    source: "https://github.com/Sivaranjani-A/react-movie-poster",
+  },
 
-    {
-        imgsrc: pro3,
-        title: "LIBRARY MANAGEMENT",
-        text: "It is a simple library management app.Login as a public,can view books,borrow books and return it.Login as a librarian add books,view customer details.",
-        view: "https://splendorous-cocada-ab437d.netlify.app/",
-        source: "https://github.com/Sivaranjani-A/react-library_management"
-    },
-    {
-        imgsrc: pro1,
-        title: "STACK OVERFLOW CLONE",
-        text: "It’s a simple stack overflow clone app, user can search answers and vote answers and also post answers using keyword",
-        view: "https://beautiful-manatee-18088e.netlify.app/",
-        source: "https://github.com/Sivaranjani-A/stackoverflowclone-frontend"
-    },
-    {
-        imgsrc: pro4,
-        title: "BLOG APP",
-        text: "It’s a simple blog app.User can add blog,edit blog and also delete blog.",
-        view: "https://cheery-cranachan-d79d30.netlify.app/",
-        source: "https://github.com/Sivaranjani-A/blogapp_frontend"
-    }
-]
+  {
+    imgsrc: pro3,
+    title: "LIBRARY MANAGEMENT",
+    text: "It is a simple library management app.Login as a public,can view books,borrow books and return it.Login as a librarian add books,view customer details.",
+    view: "https://splendorous-cocada-ab437d.netlify.app/",
+    source: "https://github.com/Sivaranjani-A/react-library_management",
+  },
+  {
+    imgsrc: pro1,
+    title: "STACK OVERFLOW CLONE",
+    text: "It’s a simple stack overflow clone app, user can search answers and vote answers and also post answers using keyword",
+    view: "https://beautiful-manatee-18088e.netlify.app/",
+    source: "https://github.com/Sivaranjani-A/stackoverflowclone-frontend",
+  },
+  {
+    imgsrc: pro4,
+    title: "BLOG APP",
+    text: "It’s a simple blog app.User can add blog,edit blog and also delete blog.",
+    view: "https://cheery-cranachan-d79d30.netlify.app/",
+    source: "https://github.com/Sivaranjani-A/blogapp_frontend",
+  },
+];
 export default ProjectCardData;
