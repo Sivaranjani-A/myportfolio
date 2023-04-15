e6a197fa6419f890701f6385dd3aeae03461fea1
Sivaranjani-A <ranjani731997@gmail.com>
2023-04-15 12:58:34 +0530

    update

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -1,28 +1,38 @@
 import "./HeroimgStyles.css";
-import IntroImg from "../assets/intro-bg.jpg"
-import resume from "../assets/sivaranjani resume.pdf"
+import IntroImg from "../assets/intro-bg.jpg";
+import resume from "../assets/sivaranjani resume.pdf";
 
-
-import React from 'react'
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import { FaDownload } from "react-icons/fa";
 
 const Heroimg = () => {
-    return (
-        <div className="hero">
-            <div className="mask">
-                <img className="intro-img" src={IntroImg} alt="IntroImg" />
-            </div>
-            <div className="content">
-                <p>HI, I'M SIVARANJANI.</p>
-                <h1>FullStack Developer.</h1>
-                <div>
-                    <Link to={"/project"} className="btn">Projects</Link>
-                    <a href={resume} download=""><button class="btn"> download CV <FaDownload size={16} style={{ color: "#fff", marginRight: "1rem" }} /></button></a>
-                </div>
-            </div>
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="intro-img" src={IntroImg} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p>HI, I'M SIVARANJANI.</p>
+        <h1>FullStack Developer.</h1>
+        <div>
+          <Link to={"/project"} className="btn">
+            Projects
+          </Link>
+          <NavLink to={resume} target="blank">
+            <button class="btn">
+              {" "}
+              download CV{" "}
+              <FaDownload
+                size={16}
+                style={{ color: "#fff", marginRight: "1rem" }}
+              />
+            </button>
+          </NavLink>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Heroimg
+export default Heroimg;
